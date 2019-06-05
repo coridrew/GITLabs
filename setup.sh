f2871c6b76e12940e2186c870e494621d899931e
Tim Rayburn <tim@timrayburn.net>
2019-06-05 09:10:45 -0400

    update to use beyond compare

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 # Configure P4Merge
 git config --global diff.tool p4merge
 git config --global difftool.p4merge.cmd "p4merge.exe \$LOCAL \$REMOTE"
-git config --global merge.tool p4merge
-git config --global mergetool.p4merge.cmd "p4merge.exe \$BASE \$LOCAL \$REMOTE \$MERGED"
-git config --global mergetool.p4merge.trustExitCode true
-git config --global mergetool.p4merge.keepBackup false
+git config --global merge.tool bc3
+# git config --global mergetool.p4merge.cmd "p4merge.exe \$BASE \$LOCAL \$REMOTE \$MERGED"
+# git config --global mergetool.p4merge.trustExitCode true
+# git config --global mergetool.p4merge.keepBackup false
+
+PATH=$PATH:'C:\Program Files\Beyond Compare 4'
 
 # Bare minimum aliases
 git config --global alias.a 'add -A'
